d8ae28ef4f039189e8c1628972fe75dfcf3d3535
Steve Hill <sghill.dev@gmail.com>
2022-06-03 09:50:51 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,99 @@
+import nebula.plugin.contacts.Contact
+import nebula.plugin.contacts.ContactsExtension
+
+plugins {
+    `java-library`
+
+    id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    id("nebula.release") version "15.3.1"
+
+    id("nebula.maven-manifest") version "17.3.2"
+    id("nebula.maven-nebula-publish") version "17.3.2"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+}
+
+apply(plugin = "nebula.publish-verification")
+
+configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
+    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
+}
+
+group = "org.openrewrite.recipe"
+description = "Rewrite recipes."
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    mavenCentral()
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+    }
+}
+
+//The bom version can also be set to a specific version or latest.release.
+val rewriteBomVersion = "latest.integration"
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:latest.release")
+    compileOnly("com.google.code.findbugs:jsr305:latest.release")
+    annotationProcessor("org.projectlombok:lombok:latest.release")
+    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:${rewriteBomVersion}"))
+
+    implementation("org.openrewrite:rewrite-java")
+    runtimeOnly("org.openrewrite:rewrite-java-11")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.assertj:assertj-core:latest.release")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+configure<ContactsExtension> {
+    val j = Contact("team@moderne.io")
+    j.moniker("Team Moderne")
+    people["team@moderne.io"] = j
+}
+
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            suppressPomMetadataWarningsFor("runtimeElements")
+        }
+    }
+}
+
+publishing {
+  repositories {
+      maven {
+          name = "moderne"
+          url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
+      }
+  }
+}
