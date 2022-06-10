d2c2e91b7fc642b87af4f2fd1566e342e6f36ecd
Steve Hill <sghill.dev@gmail.com>
2022-06-09 19:58:38 -0700

    Populate description value if creating index.jelly

--- src/main/java/net/sghill/jenkins/rewrite/CreateIndexJelly.java
+++ src/main/java/net/sghill/jenkins/rewrite/CreateIndexJelly.java
@@ -1,17 +1,23 @@
 package net.sghill.jenkins.rewrite;
 
-import org.intellij.lang.annotations.Language;
+import lombok.Getter;
 import org.openrewrite.ExecutionContext;
+import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.SourceFile;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.ListUtils;
 import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.maven.MavenIsoVisitor;
+import org.openrewrite.maven.MavenVisitor;
 import org.openrewrite.text.PlainTextParser;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Xml;
 
 import java.nio.file.PathMatcher;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class CreateIndexJelly extends Recipe {
 
@@ -43,12 +49,45 @@ public class CreateIndexJelly extends Recipe {
         }
 
         return ListUtils.flatMap(before, sourceFile -> {
-            if(Jenkins.isJenkinsPluginPom(sourceFile) != null) {
-                return Arrays.asList(sourceFile, new PlainTextParser().parse("TODO insert contents here")
+            if (Jenkins.isJenkinsPluginPom(sourceFile) != null) { // This must be an `Xml.Document`
+                DescriptionVisitor visitor = new DescriptionVisitor();
+                visitor.visitDocument((Xml.Document) sourceFile, new InMemoryExecutionContext());
+                return Arrays.asList(sourceFile, new PlainTextParser().parse(String.join("\n",
+                                "<?jelly escape-by-default='true'?>",
+                                "<div>",
+                                visitor.description(),
+                                "</div>"))
                         .get(0)
                         .withSourcePath(sourceFile.getSourcePath().resolve("../src/main/resources/index.jelly").normalize()));
             }
             return sourceFile;
         });
+    }
+
+    static class DescriptionVisitor extends MavenVisitor<ExecutionContext> {
+        private static final String FALLBACK = "Update src/main/resources/index.jelly for a better description.";
+
+        private String value = "";
+
+        public String description() {
+            return value.isEmpty() ? FALLBACK : value;
+        }
+
+        private static final XPathMatcher DESCRIPTION_MATCHER = new XPathMatcher("/project/description");
+        private static final XPathMatcher ARTIFACT_ID_MATCHER = new XPathMatcher("/project/artifactId");
+
+        @Override
+        public Xml visitTag(Xml.Tag tag, ExecutionContext executionContext) {
+            Optional<String> tagValue = tag.getValue();
+            boolean isPresent = tagValue.isPresent();
+            if (DESCRIPTION_MATCHER.matches(getCursor()) && isPresent) {
+                value = tagValue.get();
+                return tag;
+            }
+            if (ARTIFACT_ID_MATCHER.matches(getCursor()) && isPresent) {
+                value = tagValue.get();
+            }
+            return super.visitTag(tag, executionContext);
+        }
     }
 }
--- src/test/kotlin/net/sghill/jenkins/rewrite/CreateIndexJellyTest.kt
+++ src/test/kotlin/net/sghill/jenkins/rewrite/CreateIndexJellyTest.kt
@@ -27,6 +27,7 @@ class CreateIndexJellyTest : RewriteTest {
                         <version>4.40</version>
                     </parent>
                     <artifactId>my-plugin</artifactId>
+                    <description>This is my plugin's description</description>
                     <version>0.1</version>
                     <repositories>
                         <repository>
@@ -37,7 +38,143 @@ class CreateIndexJellyTest : RewriteTest {
                 </project>
             """),
             srcMainResources(
-                text(null, "TODO insert contents here") {
+                text(null, """
+                    <?jelly escape-by-default='true'?>
+                    <div>
+                    This is my plugin's description
+                    </div>
+                """) {
+                    spec -> spec.path("index.jelly")
+                }
+            )
+        )
+    )
+    @Test
+    fun createIndexJellyEmptyDescription() = rewriteRun(
+        mavenProject("plugin",
+            pomXml("""
+                <project>
+                    <parent>
+                        <groupId>org.jenkins-ci.plugins</groupId>
+                        <artifactId>plugin</artifactId>
+                        <version>4.40</version>
+                    </parent>
+                    <artifactId>my-plugin</artifactId>
+                    <description/>
+                    <version>0.1</version>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+            """),
+            srcMainResources(
+                text(null, """
+                    <?jelly escape-by-default='true'?>
+                    <div>
+                    my-plugin
+                    </div>
+                """) {
+                    spec -> spec.path("index.jelly")
+                }
+            )
+        )
+    )
+
+    @Test
+    fun createIndexJellyNoDescription() = rewriteRun(
+        mavenProject("plugin",
+            pomXml("""
+                <project>
+                    <parent>
+                        <groupId>org.jenkins-ci.plugins</groupId>
+                        <artifactId>plugin</artifactId>
+                        <version>4.40</version>
+                    </parent>
+                    <artifactId>my-plugin</artifactId>
+                    <version>0.1</version>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+            """),
+            srcMainResources(
+                text(null, """
+                    <?jelly escape-by-default='true'?>
+                    <div>
+                    my-plugin
+                    </div>
+                """) {
+                    spec -> spec.path("index.jelly")
+                }
+            )
+        )
+    )
+
+    @Test
+    fun createIndexJellyNoArtifact() = rewriteRun(
+        mavenProject("plugin",
+            pomXml("""
+                <project>
+                    <parent>
+                        <groupId>org.jenkins-ci.plugins</groupId>
+                        <artifactId>plugin</artifactId>
+                        <version>4.40</version>
+                    </parent>
+                    <version>0.1</version>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+            """),
+            srcMainResources(
+                text(null, """
+                    <?jelly escape-by-default='true'?>
+                    <div>
+                    Update src/main/resources/index.jelly for a better description.
+                    </div>
+                """) {
+                    spec -> spec.path("index.jelly")
+                }
+            )
+        )
+    )
+
+    @Test
+    fun createIndexJellyEmptyArtifact() = rewriteRun(
+        mavenProject("plugin",
+            pomXml("""
+                <project>
+                    <parent>
+                        <groupId>org.jenkins-ci.plugins</groupId>
+                        <artifactId>plugin</artifactId>
+                        <version>4.40</version>
+                    </parent>
+                    <artifactId/>
+                    <version>0.1</version>
+                    <repositories>
+                        <repository>
+                            <id>repo.jenkins-ci.org</id>
+                            <url>https://repo.jenkins-ci.org/public/</url>
+                        </repository>
+                    </repositories>
+                </project>
+            """),
+            srcMainResources(
+                text(null, """
+                    <?jelly escape-by-default='true'?>
+                    <div>
+                    Update src/main/resources/index.jelly for a better description.
+                    </div>
+                """) {
                     spec -> spec.path("index.jelly")
                 }
             )
