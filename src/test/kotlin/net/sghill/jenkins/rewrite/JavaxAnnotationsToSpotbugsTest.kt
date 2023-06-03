81182f6f5e140277cd8e5b582f73e38a7c043d8f
Steve Hill <sghill.dev@gmail.com>
2023-06-02 22:39:43 -0700

    JavaxAnnotationsToSpotbugsTest -> RewriteTest

--- src/test/kotlin/net/sghill/jenkins/rewrite/JavaxAnnotationsToSpotbugsTest.kt
+++ src/test/kotlin/net/sghill/jenkins/rewrite/JavaxAnnotationsToSpotbugsTest.kt
@@ -1,97 +1,100 @@
 package net.sghill.jenkins.rewrite
 
 import org.junit.jupiter.api.Test
-import org.openrewrite.Recipe
-import org.openrewrite.config.Environment
+import org.openrewrite.java.Assertions.java
 import org.openrewrite.java.JavaParser
-import org.openrewrite.java.JavaRecipeTest
+import org.openrewrite.test.RecipeSpec
+import org.openrewrite.test.RewriteTest
 
-class JavaxAnnotationsToSpotbugsTest : JavaRecipeTest {
-    override val parser: JavaParser = JavaParser.fromJavaVersion()
-            .classpath("jsr305", "spotbugs-annotations")
-            .build()
+class JavaxAnnotationsToSpotbugsTest : RewriteTest {
 
-    override val recipe: Recipe = Environment.builder()
-            .scanRuntimeClasspath("net.sghill.jenkins.rewrite")
-            .build()
-            .activateRecipes("net.sghill.jenkins.rewrite.JavaxAnnotationsToSpotbugs")
+    override fun defaults(spec: RecipeSpec) {
+        spec.parser(JavaParser.fromJavaVersion().classpath("jsr305", "spotbugs-annotations"))
+        spec.recipe(JavaxAnnotationsToSpotbugs())
+    }
 
     @Test
-    fun classNameChange() = assertChanged(
-            before = """
-                        import javax.annotation.Nonnull;
-                        
-                        public class A {
-                            static @Nonnull String CONSTANT = "A";
-                        }
-                        """,
-            after = """
-                        import edu.umd.cs.findbugs.annotations.NonNull;
-                        
-                        public class A {
-                            static @NonNull String CONSTANT = "A";
-                        }
-                        """
+    fun classNameChange() = rewriteRun(
+        java(
+            """
+            import javax.annotation.Nonnull;
+            
+            public class A {
+                static @Nonnull String CONSTANT = "A";
+            }
+            """, 
+            """
+            import edu.umd.cs.findbugs.annotations.NonNull;
+            
+            public class A {
+                static @NonNull String CONSTANT = "A";
+            }
+            """
+        )
     )
-    
+
     @Test
-    fun packageChange() = assertChanged(
-            before = """
-                        import javax.annotation.CheckForNull;
-                        
-                        public class A {
-                            @CheckForNull
-                            public String key() {
-                                return null;
-                            }
-                        }
-                        """,
-            after = """
-                        import edu.umd.cs.findbugs.annotations.CheckForNull;
-                        
-                        public class A {
-                            @CheckForNull
-                            public String key() {
-                                return null;
-                            }
-                        }
-                        """
+    fun packageChange() = rewriteRun(
+        java(
+            """
+            import javax.annotation.CheckForNull;
+
+            public class A {
+                @CheckForNull
+                public String key() {
+                    return null;
+                }
+            }
+            """,
+            """
+            import edu.umd.cs.findbugs.annotations.CheckForNull;
+    
+            public class A {
+                @CheckForNull
+                public String key() {
+                    return null;
+                }
+            }
+            """
+        )
     )
-    
+
     @Test
-    fun orderImports() = assertChanged(
-            before = """
-                        import javax.annotation.CheckForNull;
-                        import javax.annotation.Nonnull;
-                        import java.util.Objects;
-                        
-                        public class A {
-                            @CheckForNull
-                            public String key() {
-                                return null;
-                            }
-                            
-                            public @Nonnull String myMethod(String in) {
-                                return Objects.equals(in, "a") ? "yes" : "no";
-                            }
-                        }
-                        """,
-            after = """
-                        import edu.umd.cs.findbugs.annotations.CheckForNull;
-                        import edu.umd.cs.findbugs.annotations.NonNull;
-                        
-                        import java.util.Objects;
-                        
-                        public class A {
-                            @CheckForNull
-                            public String key() {
-                                return null;
-                            }
-                            
-                            public @NonNull String myMethod(String in) {
-                                return Objects.equals(in, "a") ? "yes" : "no";
-                            }
-                        }
-                        """
+    fun orderImports() = rewriteRun(
+        java(
+            """
+            import javax.annotation.CheckForNull;
+            import javax.annotation.Nonnull;
+            import java.util.Objects;
+
+            public class A {
+                @CheckForNull
+                public String key() {
+                    return null;
+                }
+
+                public @Nonnull String myMethod(String in) {
+                    return Objects.equals(in, "a") ? "yes" : "no";
+                }
+            }
+            """,
+            """
+            import edu.umd.cs.findbugs.annotations.CheckForNull;
+            import edu.umd.cs.findbugs.annotations.NonNull;
+
+            import java.util.Objects;
+
+            public class A {
+                @CheckForNull
+                public String key() {
+                    return null;
+                }
+
+                public @NonNull String myMethod(String in) {
+                    return Objects.equals(in, "a") ? "yes" : "no";
+                }
+            }
+            """
+        )
     )
 }
