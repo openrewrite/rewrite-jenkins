4bfda4ff93454b4d1a2b22fb6e1066a03f1de6b6
Steve Hill <sghill.dev@gmail.com>
2023-06-16 20:23:40 -0700

    Rewrite 8.1.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ dependencyResolutionManagement {
             library("junit-bom", "org.junit", "junit-bom").version {
                 strictly("[5.9.1, 6.0.0[")
             }
-            library("rewrite-bom", "org.openrewrite:rewrite-bom:8.1.0")
-            library("rewrite-recipe-bom", "org.openrewrite.recipe:rewrite-recipe-bom:2.0.0")
+            library("rewrite-bom", "org.openrewrite:rewrite-bom:8.1.2")
+            library("rewrite-recipe-bom", "org.openrewrite.recipe:rewrite-recipe-bom:2.0.1")
         }
         create("kt") {
             val kotlin = version("kotlin", "1.8.21")
