ddf6fd0683f217ef7259e46aeebfeaa62e68a460
Steve Hill <sghill.dev@gmail.com>
2023-06-16 22:57:19 -0700

    Upgrade to Parent Pom 4.66/HtmlUnit 3.3.0 Recipes

---
+++ src/test/kotlin/net/sghill/jenkins/rewrite/UpgradeParentPom466Test.kt
@@ -0,0 +1,86 @@
+package net.sghill.jenkins.rewrite
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.maven.Assertions.pomXml
+import org.openrewrite.test.RecipeSpec
+import org.openrewrite.test.RewriteTest
+
+class UpgradeParentPom466Test : RewriteTest {
+
+    override fun defaults(spec: RecipeSpec) {
+        spec.recipeFromResource("/META-INF/rewrite/rewrite.yml", "net.sghill.jenkins.rewrite.UpgradeParentPom_4_66")
+    }
+
+    @Test
+    fun noHtmlUnit() = rewriteRun(
+            pomXml(
+                    """
+                        <project>
+                            <parent>
+                                <groupId>org.jenkins-ci.plugins</groupId>
+                                <artifactId>plugin</artifactId>
+                                <version>4.65</version>
+                                <relativePath/>
+                            </parent>
+                            <artifactId>example-plugin</artifactId>
+                            <version>0.8-SNAPSHOT</version>
+                            <properties>
+                                <jenkins.version>2.375.1</jenkins.version>
+                            </properties>
+                            <repositories>
+                                <repository>
+                                    <id>repo.jenkins-ci.org</id>
+                                    <url>http://repo.jenkins-ci.org/public/</url>
+                                </repository>
+                            </repositories>
+                        </project>
+                    """, """
+                        <project>
+                            <parent>
+                                <groupId>org.jenkins-ci.plugins</groupId>
+                                <artifactId>plugin</artifactId>
+                                <version>4.66</version>
+                                <relativePath/>
+                            </parent>
+                            <artifactId>example-plugin</artifactId>
+                            <version>0.8-SNAPSHOT</version>
+                            <properties>
+                                <jenkins.version>2.375.1</jenkins.version>
+                            </properties>
+                            <repositories>
+                                <repository>
+                                    <id>repo.jenkins-ci.org</id>
+                                    <url>http://repo.jenkins-ci.org/public/</url>
+                                </repository>
+                            </repositories>
+                        </project>
+                    """
+            )
+    )
+
+    @Test
+    fun doesNotDowngrade() = rewriteRun(
+            pomXml(
+                    """
+                        <project>
+                            <parent>
+                                <groupId>org.jenkins-ci.plugins</groupId>
+                                <artifactId>plugin</artifactId>
+                                <version>4.67</version>
+                                <relativePath/>
+                            </parent>
+                            <artifactId>example-plugin</artifactId>
+                            <version>0.8-SNAPSHOT</version>
+                            <properties>
+                                <jenkins.version>2.375.1</jenkins.version>
+                            </properties>
+                            <repositories>
+                                <repository>
+                                    <id>repo.jenkins-ci.org</id>
+                                    <url>http://repo.jenkins-ci.org/public/</url>
+                                </repository>
+                            </repositories>
+                        </project>
+                    """            )
+    )
+}
