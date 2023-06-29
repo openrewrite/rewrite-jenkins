4ad665aa33b031e2652e73ac257142f5fff5c2ad
Steve Hill <sghill.dev@gmail.com>
2023-06-28 18:57:27 -0700

    Relocate UpgradeHtmlUnit_3_3_0, JavaxAnnotationsToSpotbugs
    Rewrite test in java. Issue #4

---
+++ src/test/java/org/openrewrite/jenkins/JavaxAnnotationsToSpotBugsTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class JavaxAnnotationsToSpotBugsTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().classpath("jsr305", "spotbugs-annotations"));
+        spec.recipeFromResource("/META-INF/rewrite/rewrite.yml", "org.openrewrite.jenkins.JavaxAnnotationsToSpotbugs");
+    }
+
+    @Test
+    void shouldChangeClassName() {
+        rewriteRun(java(
+                """
+                        import javax.annotation.Nonnull;
+                                    
+                        public class A {
+                            static @Nonnull String CONSTANT = "A";
+                        }
+                        """.stripIndent(),
+                """
+                        import edu.umd.cs.findbugs.annotations.NonNull;
+                                    
+                        public class A {
+                            static @NonNull String CONSTANT = "A";
+                        }
+                        """.stripIndent()
+        ));
+    }
+
+    @Test
+    void shouldChangePackage() {
+        rewriteRun(java(
+                """
+                        import javax.annotation.CheckForNull;
+
+                        public class A {
+                            @CheckForNull
+                            public String key() {
+                                return null;
+                            }
+                        }
+                        """.stripIndent(),
+                """
+                        import edu.umd.cs.findbugs.annotations.CheckForNull;
+                            
+                        public class A {
+                            @CheckForNull
+                            public String key() {
+                                return null;
+                            }
+                        }
+                        """.stripIndent()
+        ));
+    }
+
+    @Test
+    void shouldOrderImports() {
+        rewriteRun(java(
+                """
+                        import javax.annotation.CheckForNull;
+                        import javax.annotation.Nonnull;
+                        import java.util.Objects;
+                                        
+                        public class A {
+                            @CheckForNull
+                            public String key() {
+                                return null;
+                            }
+                                        
+                            public @Nonnull String myMethod(String in) {
+                                return Objects.equals(in, "a") ? "yes" : "no";
+                            }
+                        }
+                        """.stripIndent(),
+                """
+                        import edu.umd.cs.findbugs.annotations.CheckForNull;
+                        import edu.umd.cs.findbugs.annotations.NonNull;
+                                        
+                        import java.util.Objects;
+                                        
+                        public class A {
+                            @CheckForNull
+                            public String key() {
+                                return null;
+                            }
+                                        
+                            public @NonNull String myMethod(String in) {
+                                return Objects.equals(in, "a") ? "yes" : "no";
+                            }
+                        }
+                        """.stripIndent()
+        ));
+    }
+}
