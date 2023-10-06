46bf237ff54ef7dc1259d16fbb3ec86d41f53e48
gounthar <gounthar@gmail.com>
2023-10-06 22:17:49 +0200

    chore(dependencies): Move to JDK21
    The default archetype has changed:  https://github.com/jenkinsci/archetypes/blob/master/common-files/Jenkinsfile

--- src/test/java/org/openrewrite/jenkins/ModernizeJenkinsfileTest.java
+++ src/test/java/org/openrewrite/jenkins/ModernizeJenkinsfileTest.java
@@ -55,10 +55,15 @@ class ModernizeJenkinsfileTest implements RewriteTest {
               </project>
               """),
           text(null, """
-              buildPlugin(useContainerAgent: true, configurations: [
-                [ platform: 'linux', jdk: '11' ],
-                [ platform: 'windows', jdk: '11' ],
-                [ platform: 'linux', jdk: '17' ],
+              /*
+               See the documentation for more options:
+               https://github.com/jenkins-infra/pipeline-library/
+              */
+              buildPlugin(
+                useContainerAgent: true, // Set to `false` if you need to use Docker for containerized tests
+                configurations: [
+                  [platform: 'linux', jdk: 21],
+                  [platform: 'windows', jdk: 17],
               ])""".stripIndent(),
             spec -> spec.path("Jenkinsfile")));
     }
